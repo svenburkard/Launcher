36bee2e2361be183ef71d73df825bcfcb44e0af7
Sven Burkard <dev@sven-burkard.de>
2026-02-01 16:04:22 +0100

    feat(db): add user serial to launcher entities

--- app/src/main/kotlin/org/fossify/home/databases/AppsDatabase.kt
+++ app/src/main/kotlin/org/fossify/home/databases/AppsDatabase.kt
@@ -5,6 +5,8 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
+import androidx.room.migration.Migration
+import androidx.sqlite.db.SupportSQLiteDatabase
 import org.fossify.home.helpers.Converters
 import org.fossify.home.interfaces.AppLaunchersDao
 import org.fossify.home.interfaces.HiddenIconsDao
@@ -15,7 +17,7 @@ import org.fossify.home.models.HomeScreenGridItem
 
 @Database(
     entities = [AppLauncher::class, HomeScreenGridItem::class, HiddenIcon::class],
-    version = 5
+    version = 6
 )
 @TypeConverters(Converters::class)
 abstract class AppsDatabase : RoomDatabase() {
@@ -28,6 +30,26 @@ abstract class AppsDatabase : RoomDatabase() {
 
     companion object {
         private var db: AppsDatabase? = null
+        private val MIGRATION_5_6 = object : Migration(5, 6) {
+            override fun migrate(database: SupportSQLiteDatabase) {
+                database.execSQL(
+                    "ALTER TABLE apps ADD COLUMN user_serial INTEGER NOT NULL DEFAULT 0"
+                )
+                database.execSQL(
+                    "ALTER TABLE home_screen_grid_items ADD COLUMN user_serial INTEGER NOT NULL DEFAULT 0"
+                )
+                database.execSQL(
+                    "ALTER TABLE hidden_icons ADD COLUMN user_serial INTEGER NOT NULL DEFAULT 0"
+                )
+                database.execSQL(
+                    "DROP INDEX IF EXISTS index_apps_package_name"
+                )
+                database.execSQL(
+                    "CREATE UNIQUE INDEX IF NOT EXISTS index_apps_package_name_activity_name_user_serial " +
+                            "ON apps(package_name, activity_name, user_serial)"
+                )
+            }
+        }
 
         fun getInstance(context: Context): AppsDatabase {
             if (db == null) {
@@ -37,7 +59,8 @@ abstract class AppsDatabase : RoomDatabase() {
                             context.applicationContext,
                             AppsDatabase::class.java,
                             "apps.db"
-                        ).build()
+                        ).addMigrations(MIGRATION_5_6)
+                            .build()
                     }
                 }
             }
